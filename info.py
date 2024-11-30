f0d60ea162566c968d8cf0e28a2056b4d5499520
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-11-30 10:33:33 -0800

    Update info.py

--- info.py
+++ info.py
@@ -179,8 +179,8 @@ DIRECT_GEN_URL = os.environ.get("DIRECT_GEN_URL", "https://web-ikkst7anqzvo.up-d
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
 
 POST_MODE= bool(environ.get('POST_MODE', True))
-POST_SHORT_API = environ.get('POST_SHORT_API', '69bfe45fc35b6b3178b4b95de9ef1db14a746ce7')
-POST_SHORT_URL = environ.get('POST_SHORT_URL', 'Modijiurl.com')
+POST_SHORT_API = environ.get('POST_SHORT_API', '')
+POST_SHORT_URL = environ.get('POST_SHORT_URL', '')
 
 HOW_TO_POST_SHORT = environ.get('HOW_TO_POST_SHORT', 'https://t.me/How_To_Get_Movie')
 
